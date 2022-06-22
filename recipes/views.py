7185efdc34542b78e55ce7e9935160c21e62405d
Waldir J. Pereira Junior <waldirpereira@gmail.com>
2022-06-21 22:54:31 -0300

    using templates with temp

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Waldir'
+    })
 
 
 def about(request):
-    return HttpResponse('ABOUT')
+    return render(request, 'delete-me/temp.html')
 
 
 def contact(request):
