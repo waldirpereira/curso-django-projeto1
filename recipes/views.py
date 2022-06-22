ab3e3ae25c703f59036d4cedb3491c14aed42103
Waldir J. Pereira Junior <waldirpereira@gmail.com>
2022-06-22 17:00:48 -0300

    removing contact and about views

--- recipes/views.py
+++ recipes/views.py
@@ -7,11 +7,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Waldir'
     })
-
-
-def about(request):
-    return render(request, 'recipes/about.html')
-
-
-def contact(request):
-    return render(request, 'recipes/contact.html')
